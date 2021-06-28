842306635594472ded9a8c91c3777a416975b862
DavidGlezQ <62410785+DavidGlezQ@users.noreply.github.com>
2021-06-28 16:41:26 -0500

    6-28-2021-Seccion2-Clase7
    Primer ejemplo con Holo, antes de material design.

--- app/src/main/java/com/davidglez/a6_28_2021_seccion2udemyandroidux_ui/MainActivity.java
+++ app/src/main/java/com/davidglez/a6_28_2021_seccion2udemyandroidux_ui/MainActivity.java
@@ -1,10 +1,9 @@
 package com.davidglez.a6_28_2021_seccion2udemyandroidux_ui;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
