cea268fa0b746271fdc05ada7c11bb13cbc25bb2
DavidGlezQ <62410785+DavidGlezQ@users.noreply.github.com>
2021-06-28 17:59:08 -0500

    6-28-2021-FinSeccion2-Clase12
    Aplicación de Material Design y Material Theming

--- app/src/main/java/com/davidglez/a6_28_2021_seccion2udemyandroidux_ui/MainActivity.java
+++ app/src/main/java/com/davidglez/a6_28_2021_seccion2udemyandroidux_ui/MainActivity.java
@@ -3,7 +3,9 @@ package com.davidglez.a6_28_2021_seccion2udemyandroidux_ui;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class MainActivity extends Activity {
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
